0325058920ce81c16e6fdacf62e3c166d399361e
Juan Pablo <juanpasan2000@gmail.com>
2024-05-05 14:24:46 -0500

    Using Spring Data JPA

--- src/main/java/com/nexus/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/nexus/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.nexus.springboot.learnjpaandhibernate.course;
 
 import com.nexus.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
 import com.nexus.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.nexus.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,23 +10,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 
-//    @Autowired
-//    private CourseJdbcRepository repository;
-
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1L, "Learn Angular", "Udemy"));
-        repository.insert(new Course(2L, "Learn Python", "Udemy"));
-        repository.insert(new Course(3L, "Learn MongoDB", "Udemy"));
+        repository.save(new Course(1L, "Learn Angular", "Udemy"));
+        repository.save(new Course(2L, "Learn Python", "Udemy"));
+        repository.save(new Course(3L, "Learn MongoDB", "Udemy"));
 
         repository.deleteById(3L);
 
         System.out.println(repository.findById(1L));
         System.out.println(repository.findById(2L));
-
     }
 
 }
